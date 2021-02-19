538ad551e67831296050e991d4a01a5bcccb762a
Vitor Vilela <vitor.vilela.mario@gmail.com>
2021-02-19 18:24:25 -0300

    Optimize rendering more

--- Race-Drivin/asm/render_speedup.asm
+++ Race-Drivin/asm/render_speedup.asm
@@ -87,5 +87,144 @@ org $00CE29
 	
 org $00CE0E
 	++
+	
+;;;;;;;;;;;;;;;;;;;;;;;;
+;00ceb7
+
+org $00CEAB
+	BNE +
+	
+org $00CEB3
+	NOP #3
+	
++
+	LDA $C0
+	CMP $C2
+	BEQ +
+	BPL ++
+	
+;print pc
+warnpc $00CEBE
+	
+org $00CEF4
+	+
+	
+org $00CED9
+	++
+	
+;;;;;;;;;;;;;;;;;;;;;;;;
+;00cd8b
+
+org $00CD76
+	BNE +
+	
+org $00CD82
+	NOP #7
++
+	LDA $BE
+	CLC
+	ADC $C2
+	BMI +
+	BEQ ++
+;print pc
+warnpc $00CD92
+	
+org $00CDAD
+	+
+
+org $00CDC8
+	++
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;00be57
+
+org $00BE4C
+	LDA $38
+	BPL +
+	CLC
+	ADC $3C
+	BPL +
+	STZ $9E
+	BRA ++
+	
+org $00BE60
+	+
+org $00BE62
+	++
+	
+	
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;00BD0E
+
+org $00BD0B
+	CMP #$0001
+	BPL +
+	INC $9C
++	LDA $6506,y
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;00bd1b
+
+;org $00BD18
+	CMP #$0001
+	BPL +
+	LDA #$0002
+	TSB $9C
+	BRA +
+;print pc
+warnpc $00BD27
+	
+org $00BD27
+	+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;00BE69
+
+org $00BE62
+	LDA $3A
+	BMI +
+	CMP $3C
+	BMI +
+	BEQ +
+	STZ $A0
+	BRA ++
+
+org $00BE74
+	+
+org $00BE76
+	++
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;00C1A0 e 00C1B2
+
+org $00C195
+	LDA $64FC,y
+	CLC
+	ADC $64FE,y
+	BPL +
+	INC $AC
++
+	LDA $6504,y
+	CLC
+	ADC $6506,y
+	BPL +
+	LDA #$0002
+	TSB $AC
++
+	LDA $AC
+	CMP #$0003
+	BNE +
+	JMP ++
+	
+	NOP #16
+
+;print pc
+warnpc $00C1C8
+
+org $00C1C8
+	+
+
+org $00C25E
+	++
 
 pullpc
